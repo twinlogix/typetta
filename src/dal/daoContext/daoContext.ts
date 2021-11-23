89f7437b8858a86e8b358bd674dfbf7cbfab18ea
Mattia Minotti <mattia@twinlogix.com>
2021-11-23 17:34:50 +0100

    First compiling generation version.

--- src/dal/daoContext/daoContext.ts
+++ src/dal/daoContext/daoContext.ts
@@ -2,10 +2,13 @@ import { AbstractDAO } from '../dao/dao'
 import { DataTypeAdapter } from '../drivers/drivers.types'
 
 export abstract class AbstractDAOContext {
+
   public adapters: Map<any, DataTypeAdapter<any, any, any>>
-  public constructor(adapters: Map<any, DataTypeAdapter<any, any, any>>) {
-    this.adapters = adapters
+
+  public constructor(adapters?: Map<any, DataTypeAdapter<any, any, any>>) {
+    this.adapters = adapters || new Map();
   }
+
   public dao(daoName: string): AbstractDAO<any, any, any, any, any, any, any, never, any, any> {
     return (this as any)[daoName]
   }
